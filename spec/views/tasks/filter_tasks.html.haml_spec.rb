e325bf10eb672a4b7ca79ad91a74d1f6dd2e1af9
Kerz <kerzo@mail.ru>
2015-09-06 10:52:19 +0300

    fix assign_task(add status)

--- spec/views/tasks/filter_tasks.html.haml_spec.rb
+++ spec/views/tasks/filter_tasks.html.haml_spec.rb
@@ -5,49 +5,63 @@ RSpec.describe "tasks/index", type: :view do
   	let(:admin) {FactoryGirl.create(:admin, invited: true)}
   	let(:user_with_org) {FactoryGirl.create(:user_with_org, invited: true, organization_id: admin.organization_id)}
 	let(:user_with_org2) {FactoryGirl.create(:user_with_org, invited: true, organization_id: admin.organization_id)}		
-  	let!(:task) {admin.assign_task(user_with_org, "task_name", date_exec: "2016-08-10")}
-  	let!(:task2) {admin.assign_task(user_with_org2, "task_name2", date_exec: "2016-08-10")}
+  	let!(:task) {admin.assign_task(user_with_org, "task_name1", date_exec: "2016-08-10")}
+  	let!(:task2) {admin.assign_task(user_with_org2, "task_name2", date_exec: "2016-08-10", status: "pause")}
+  	let!(:task3) {user_with_org.assign_task(admin, "task_name3", date_exec: "2016-08-11", status: "archived")}
+  	let!(:task4) {user_with_org2.assign_task(admin, "task_name4", date_exec: "2016-08-14", status: "complete")}
   	let!(:tasks) {Task.collect_tasks("2016-08-10", admin, only_day: true)}
 
   	subject { page }
 
-  			describe "click all tasks" , js:true do
+  			describe "click all tasks and filter", js:true do
 			before do
 				view_daytime_tasks admin
 
-		    	# within "#buttons_container" do 
-		     #      first(:link, "all_tasks").click
-		    	# end
 		    	click_link("Все задачи")
 		    	click_link("Отфильтровать")
 		    end
 
-		    it "and filter by executor" do
+		    it "by manager" do
 		    	select("Менеджер", from: "task_your_status")
 		    	click_button("Отправить")
-		    	pending "should be tasks only one user"
+		    	expect(page).to have_content(task.name)
+		    	expect(page).not_to have_content(task3.name)
 		    end
 
-		    it "and filter by date" do
-		    	select("2015", from: "tasks_date_exec_start_1i")
+		    it "by executor" do
+		    	select("Исполнитель", from: "task_your_status")
 		    	click_button("Отправить")
-		    	pending "should not be tasks only within chosen date"
-
-		    	select("2016", from: "tasks_date_exec_start_1i")
-		    	click_button("Отправить")
-		    	pending "should be tasks only within chosen date"
+		    	expect(page).to have_content(task3.name)
+		    	expect(page).not_to have_content(task.name)
 		    end
 
-		    it "and filter by task name" do
+		    it "by start_date" do
+		    	fill_in "Начиная с:", with:"2016-08-11"
+		    	click_button("Отправить")
+		    	expect(page).to have_content(task3.name)
+		    	expect(page).not_to have_content(task2.name)
+		    end
+
+		    it "by end_date" do
+		    	fill_in "Заканчивая до:", with: "2016-08-12"
+		    	click_button("Отправить")
+		    	expect(page).to have_content(task.name)
+		    	expect(page).not_to have_content(task4.name)
+		    end
+
+		    it "by task name" do
 		    	fill_in "Название задачи:", with: task.name
 		    	click_button("Отправить")
-		    	pending "should be tasks only with chosen name"
+		    	expect(page).to have_content(task.name)
 		    end
 
-		    it "and filter by task status" do
+		    it "by task status" do
 		    	select "Не выполняется", from: "filter_task_status"
 		    	click_button("Отправить")
-		    	pending "should be tasks only with chosen name"
+		    	expect(page).to have_content(task.name)
+		    	expect(page).not_to have_content(task2.name)
+		    	expect(page).not_to have_content(task3.name)
+		    	expect(page).not_to have_content(task4.name)
 		    end
 		end
   end
