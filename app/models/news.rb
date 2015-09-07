6dcfa4982871117e13ba84692617117b8049c790
Kerz <kerzo@mail.ru>
2015-09-07 23:57:22 +0300

    fix method create_news and fix news model spec

--- app/models/news.rb
+++ app/models/news.rb
@@ -11,6 +11,11 @@ class News < ActiveRecord::Base
   def self.create_news(news_object, reason)
   	if reason == :new_task
   		news_object.news_due_task.create(reason: reason, user_id: news_object.manager.id)
+  	elsif reason == :new_user
+  		users = news_object.organization.users
+  		users.each do |u| 
+  			u.news_due_user.create(reason: reason, user_id: u.id) unless u == news_object
+  		end
   	end
   end
 
--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -3,26 +3,34 @@ require 'rails_helper'
 RSpec.describe News, type: :model do
 
    it{is_expected.to respond_to(:readed)}
-   it{is_expected.to respond_to(:object)}
+   it{is_expected.to respond_to(:target)}
+   it{is_expected.to respond_to(:user)}
    it{is_expected.to respond_to(:reason)}
-   let(:user) { FactoryGirl.create(:user) }
-   let(:user_news) { user.news.create(body: "It's first news")}
+   let(:user_with_org) { FactoryGirl.create(:user_with_org) }
+   let(:admin) { FactoryGirl.create(:admin) }
+   let!(:task) {admin.assign_task(user_with_org, date_exec: "2016-10-10")}
+   let!(:news_due_task) { News.create_news(task, :new_task)}
+   let!(:news_due_user) { News.create_news(user_with_org, :new_user); admin.news.last}
 
-   it "user should have news" do
-   	expect(user.news).to include(user_news)
+   it "users should have news when new_user" do
+   	expect(admin.news).to include(news_due_user)
+   end
+
+   it "invited user should not have news about new_user" do
+   	expect(user_with_org.news).not_to include(news_due_user)
    end
 
    it "created news should not be readed" do
-   	expect(user_news.readed).to be_falsey
+   	expect(news_due_user.readed).to be_falsey
    end
 
    it "name should not be empty" do
-   	invalid_news = user.news.create
+   	invalid_news = admin.news_due_user.create
    	expect(invalid_news).not_to be_valid
    end
 
    it "when #read" do
-   	expect{user_news.read}.to change{user_news.readed}.from(false).to(true) 
+   	expect{news_due_user.read}.to change{news_due_user.readed}.from(false).to(true) 
    end
 
 end
