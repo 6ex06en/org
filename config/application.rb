8fa12abe49389093601c520a7ec92707e183b7ec
Kerz <kerzo@mail.ru>
2016-03-15 07:26:09 +0300

    delete unnecessary congif sttring

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,5 @@ module Organizer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths << "#{Rails.root}/app/private_message"
   end
 end
